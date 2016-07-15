7fd59c9294320d05324b113318b3018272337e53
Dyonovan <lookinforfun0000@gmail.com>
2016-07-15 18:59:26 -0400

    Added Notes section

--- src/main/java/com/dyonovan/crashlogadditions/CrashLogAdditions.java
+++ src/main/java/com/dyonovan/crashlogadditions/CrashLogAdditions.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  * @author Dyonovan
  * @since 7/14/2016
  */
-@Mod(name = "Crash Log Additions", modid = "crashlogadditions", version = "1.1", dependencies = "required-after:Forge@[11.14.4.1563,)")
+@Mod(name = "Crash Log Additions", modid = "crashlogadditions", version = "1.1", dependencies = "required-after:Forge@[11.15.1.1722,)")
 public class CrashLogAdditions {
 
     @Mod.EventHandler
